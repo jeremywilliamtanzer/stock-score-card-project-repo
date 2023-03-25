73f68779e18d0e41e97f3d045decfd50f4c7fd0b
jeremy tanzer <jeremywilliam.tanzer@ehl.ch>
2023-03-25 15:44:35 +0100

    done final fix, pushing last version to gh for the weekend

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import nltk
 nltk.download('vader_lexicon')
 #from params import *
 
-#poly_key = st.secrets["POLY_KEY"]
+poly_key = st.secrets["POLY_KEY"]
 #alpha_key = st.secrets["ALPHA_KEY"]
 #news_key = st.secrets["NEWS_KEY"]
 
@@ -21,12 +21,13 @@ ticker = st.text_input('Insert Ticker here')
 ticker = ticker.upper()
 
 if ticker != "":
-    company_logo, company_name, company_sector = requests.get('https://ssc-cont-ifhzucuzaa-ew.a.run.app/get_ticker_details?tickers=' + ticker)
+    company_logo, company_name, company_sector = requests.get('https://ssc-cont-ifhzucuzaa-ew.a.run.app/get_ticker_details?tickers=' + ticker).json()
+    company_logo = f'{company_logo}?apiKey={poly_key}'
     growth = round(requests.get('https://ssc-cont-ifhzucuzaa-ew.a.run.app/get_aggregate?tickers=' + ticker).json()['growth'], 2)
-    div_yield = requests.get('https://ssc-cont-ifhzucuzaa-ew.a.run.app/get_dividend_yield?tickers=' + ticker).json()['Dividend Yield:']
     mkt_cap = requests.get('https://ssc-cont-ifhzucuzaa-ew.a.run.app/mkt_cap?tickers=' + ticker).json()['market_capitalization']
-
     ratios = requests.get('https://ssc-cont-ifhzucuzaa-ew.a.run.app/get_ratios?tickers=' + ticker).json()
+
+    dividend_yield = ratios['Dividend_yield']
     payout = ratios["Payout_ratio"]
     eps_next_year = ratios["EPS_next_1Y"]
     eps_past_five_years = ratios["EPS_past_5Y"]
@@ -49,5 +50,5 @@ if ticker != "":
             st.markdown(f'EPS Past 5Y: {eps_past_five_years}%')
             st.markdown(f'EPS Next Year: {eps_next_year}')
             st.markdown(f'Debt/FCF: {debt_over_fcf}')
-            st.markdown(f'Divident Yield: {div_yield}')
+            st.markdown(f'Divident Yield: {dividend_yield}')
             st.markdown(f'Market Cap: {mkt_cap}')
