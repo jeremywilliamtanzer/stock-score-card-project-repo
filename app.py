9e6a4177a6faf2dd63abcdc61842cdc4c89e668f
Nuno Esguelha <nesguelha@gmail.com>
2023-03-29 21:52:06 +0200

    Finally working with icons

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import requests
 from datetime import *
 import datetime
 import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
 
 # poly_key = st.secrets["POLY_KEY"]
@@ -18,9 +17,8 @@ def get_stock_price_history(tickers):
     """Get stock history for a given stock
     over a 2 year date range in yearly timespan.
     """
-    POLY_KEY_1 = 'rW1fMPt5T8N4lrq6J4HM58LKZj1VBoPl'
     tickers = tickers.upper()
-
+    POLY_KEY_1 = 'wQ5FjyMjpTSO2j5vBxbLuIp72hwYd5E5'
     # Define the API URL and parameters
     end = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
     start = (date.today() - timedelta(days=2*365)).strftime("%Y-%m-%d")
@@ -43,7 +41,8 @@ if ticker != "":
     # growth = round(requests.get('https://ssc-cont-ifhzucuzaa-ew.a.run.app/get_aggregate?tickers=' + ticker).json()['growth'], 2)
     # mkt_cap = requests.get('https://ssc-cont-ifhzucuzaa-ew.a.run.app/mkt_cap?tickers=' + ticker).json()['market_capitalization']
     # ratios = requests.get('https://ssc-cont-ifhzucuzaa-ew.a.run.app/get_ratios?tickers=' + ticker).json()
-    # # latest_price = requests.get('https://ssc-cont-ifhzucuzaa-ew.a.run.app/get_stock_price?tickers=' + ticker).json()["current_price"]
+    # latest_price = requests.get('https://ssc-cont-ifhzucuzaa-ew.a.run.app/get_stock_price?tickers=' + ticker).json()["current_price"]
+    # news_sentiment = requests.get('https://ssc-cont-ifhzucuzaa-ew.a.run.app/news_score?tickers=' + ticker).json()["current_price"]
 
     # dividend_yield = ratios['Dividend_yield']
     # payout = ratios["Payout_ratio"]
@@ -51,37 +50,66 @@ if ticker != "":
     # eps_past_five_years = ratios["EPS_past_5Y"]
     # debt_over_fcf = ratios["Debt_over_FCF"]
 
-    # st.image(company_logo, width = 100)
-    # a 2x2 grid the long way
-    # with st.container():
-    #     col1, col2 = st.columns(2)
-    #     with col1:
-    #         st.markdown('Hello World')
-    #         st.markdown(company_name)
-    #         st.markdown(company_sector)
-    #         st.markdown(f'Market Cap: USD{mkt_cap}MM')
-    #         st.markdown(f'Stock Price: USD{latest_price}')
-    #     with col2:
-    #         st.markdown('Hello World')
-    #         st.markdown(f'Sales Past 5Y: {growth}%')
-    #         st.markdown(f'EPS Past 5Y: {eps_past_five_years}%')
-    #         st.markdown(f'EPS Next Year: {eps_next_year}%')
-    #         st.markdown(f'Debt/FCF: {debt_over_fcf} years')
-    #         st.markdown(f'Divident Yield: {dividend_yield}%')
-    # st.plotly_chart(get_stock_price_history(ticker))
+    mkt_cap = 1736
+    latest_price = 270
+    dividend_yield = 3
+    growth = 10
+    payout = 46
+    eps_next_year = 14.45
+    eps_past_five_years = 10.45
+    debt_over_fcf = 5
+    news_sentiment = 78
+
+    if dividend_yield > 2:
+        icon_1 = '✅'
+    else:
+        icon_1 = '❌'
+
+    if growth > 12:
+        icon_2 = '✅'
+    else:
+        icon_2 = '❌'
+
+    if payout > 60:
+        icon_3 = '✅'
+    else:
+        icon_3 = '❌'
+
+    if eps_next_year > 10:
+        icon_4 = '✅'
+    else:
+        icon_4 = '❌'
+
+    if eps_past_five_years > 7:
+        icon_5 = '✅'
+    else:
+        icon_5 = '❌'
+
+    if debt_over_fcf < 4:
+        icon_6 = '✅'
+    else:
+        icon_6 = '❌'
+
+    if news_sentiment > 50:
+        icon_7 = '✅'
+    else:
+        icon_7 = '❌'
 
     st.image(company_logo, width = 100)
+    # a 2x2 grid the long way
     with st.container():
         col1, col2 = st.columns(2)
         with col1:
-            st.markdown('Microsoft Corporation')
-            st.markdown('Prepackaged Software')
-            st.markdown('Market Cap: USD 10000MM')
-            st.markdown('Stock Price: USD 100')
+            st.markdown(company_name)
+            st.markdown(company_sector)
+            st.markdown(f'Market Cap: USD{mkt_cap}MM')
+            st.markdown(f'Stock Price: USD{latest_price}')
         with col2:
-            st.markdown('Sales Past 5Y: 5%')
-            st.markdown('EPS Past 5Y: 10%')
-            st.markdown('EPS Next Year: 10%')
-            st.markdown('Debt/FCF: 4 years')
-            st.markdown('Divident Yield: 3%')
+            st.markdown(f'Divident Yield: {dividend_yield}% {icon_1}', unsafe_allow_html=True)
+            st.markdown(f'Sales Past 5Y: {growth}% {icon_2}', unsafe_allow_html=True)
+            st.markdown(f'Payout Ratio: {payout}% {icon_3}', unsafe_allow_html=True)
+            st.markdown(f'EPS Next Year: {eps_next_year}% {icon_4}', unsafe_allow_html=True)
+            st.markdown(f'EPS Past 5Y: {eps_past_five_years}% {icon_5}', unsafe_allow_html=True)
+            st.markdown(f'Debt/FCF: {debt_over_fcf} years {icon_6}', unsafe_allow_html=True)
+            st.markdown(f'News Sentiment: {news_sentiment}% {icon_7}', unsafe_allow_html=True)
     st.plotly_chart(get_stock_price_history(ticker))
